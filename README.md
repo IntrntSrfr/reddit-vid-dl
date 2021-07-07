8ed03e67f00e252754faa33ba199f179ffbeb833
IntrntSrfr <aleksander@tosbakken.com>
2021-07-07 21:23:44 +0200

    added check to see if post contains video

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Reddit-vid-dl
 
 Download videos from Reddit quickly and easily
+
+- [x] Download videos from perfectly formatted Reddit links
+- [ ] Download videos from crossposts
+- [ ] Download videos from shortened URL
+- [x] Check if a post is valid for download
+- [ ] Change from os.system to subprocess?
--- main.py
+++ main.py
@@ -4,30 +4,60 @@ import sys
 
 import requests
 
-full_regex = re.compile("http(s)?([^\s]+)reddit\.com\/r\/([^\s]+)\/comments\/([^\s]+)\/([^\s]+)")
+full_regex = re.compile("^http(s)?([^\s]+)reddit\.com\/r\/([^\s]+)\/comments\/([^\s]+)\/([^\s]+)")
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 
 
 def validate_link(link: str) -> bool:
     return True
 
 
+def validate_json(j: str):
+    pass
+
 def get_media_links(url: str) -> tuple[str, str, str]:
     link_json = url + '.json'
-    res = requests.get(link_json, headers={'User-Agent': 'hehe'})
+    res = requests.get(link_json, headers={'User-Agent': user_agent})
+
+    # if request failed
+    if not res.ok:
+        raise requests.HTTPError
+
     res_json = res.json()
-    post_info = res_json[0]['data']['children'][0]['data']
-    video_url = post_info['secure_media']['reddit_video']['fallback_url']
-    audio_url = post_info['url'] + '/DASH_audio.mp4'
-    out = post_info[id]+'.mp4'
+
+    post = res_json[0]['data']['children'][0]['data']
+
+    if not post['is_video']:
+        raise ValueError('post does not contain video')
+
+    if not post['is_reddit_media_domain']:
+        raise ValueError('post is not Reddit video')
+
+    media = post['secure_media']
+
+    if 'reddit_video' not in media:
+        raise ValueError('post is not Reddit video')
+
+    video_url = media['reddit_video']['fallback_url']
+    audio_url = post['url'] + '/DASH_audio.mp4'
+    out = post['id']+'.mp4'
+
     return video_url, audio_url, out
 
 
 def download(url: str, **kwargs) -> None:
+
     if not validate_link(url):
         raise Exception("invalid reddit link")
+
+    # get the links and outfile
     video_url, audio_url, out = get_media_links(url)
+
+    # if there is an outfile specified, just override the name from the post
     if 'outfile' not in kwargs:
         out = kwargs['outfile']
+
+    # use ffmpeg to save file
     os.system('ffmpeg -i {} -i {} -c copy {}'.format(video_url, audio_url, out))
 
 
@@ -35,12 +65,14 @@ test_case = "https://reddit.com/r/PublicFreakout/comments/oey7po/iranian_woman_n
 
 if __name__ == '__main__':
     inp_url = sys.argv[1]
-    f = sys.argv[2]
+    f = ''
+    if len(sys.argv) >= 3:
+        f = sys.argv[2]
 
     if not f.endswith('.mp4'):
         f += '.mp4'
 
-    try:
-        download(inp_url, f)
-    except Exception as e:
-        print(e)
+        download(inp_url, outfile='weed.mp4')
+    #try:
+    #except Exception as e:
+    #    print(e)
